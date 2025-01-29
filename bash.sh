4fa8aaa45b16752bdbde9199d5bf8a3825e0e7f6
Saraf Anzum Shreya <96836008+SarafAnzumShreya@users.noreply.github.com>
2025-01-29 14:42:32 +0600

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# build.sh
-
-# Install PortAudio development library
-sudo apt-get update
-sudo apt-get install -y portaudio19-dev
-
-# Install Python dependencies
-pip install -r requirements.txt
