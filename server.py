97de3d9c9eb1c81831a73143929f5939981740a4
Saraf Anzum Shreya <96836008+SarafAnzumShreya@users.noreply.github.com>
2025-02-13 10:54:08 +0600

    Update server.py

--- server.py
+++ server.py
@@ -81,4 +81,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
